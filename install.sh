19600af16184c05ed4b8beeae7861ee8d50b7374
ash <anaa.ceban@gmail.com>
2025-04-13 21:45:00 +0200

    modif

--- install.sh
+++ install.sh
@@ -6,27 +6,7 @@ if [ "$dist_name" == "LinuxMint" ]; then
     sudo apt install -y make gcc pkg-config neovim openssl
 
 elif [ "$dist_name" == "ManjaroLinux" ]; then
-	  if ! pacman -Qs make &> /dev/null; then
-		  echo "installing make"
-
-        sudo pacman -S --noconfirm make
-    fi
-    if ! pacman -Qs gcc &> /dev/null; then
-		  echo "installing gcc"
-        sudo pacman -S --noconfirm gcc
-    fi
-    if ! pacman -Qs pkgconf &> /dev/null; then
-		  echo "installing pkgconf"
-        sudo pacman -S --noconfirm pkgconf
-    fi
-    if ! pacman -Qs neovim &> /dev/null; then
-		  echo "installing neovim"
-        sudo pacman -S --noconfirm neovim
-    fi
-    if ! pacman -Qs openssl &> /dev/null; then
-		  echo "installing openssl"
-        sudo pacman -S --noconfirm openssl
-    fi
+      sudo pacman -S gcc pkgconf neovim make openssl
 else
     echo "Unsupported distribution."
 fi
@@ -38,4 +18,4 @@ echo "generating certificate"
 ./src/generate_ca.sh
 ./src/generate_orchestrator_cert.sh
 ./src/generate_agent_cert.sh
-
+make
