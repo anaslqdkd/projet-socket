15eb54560c6f458c6a2c0a374fcdadb91f2cdb6c
ash <anaa.ceban@gmail.com>
2025-04-13 23:33:56 +0200

    nmap scanner

--- src/agent.c
+++ src/agent.c
@@ -1,4 +1,6 @@
 #include <netinet/in.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,154 +8,193 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
 
-#define BUFFER_SIZE 1000
+#define BUFFER_SIZE 100000
 
-void func(SSL* ssl) {
+void run_scanner (SSL* ssl, char* command) {
+    FILE* fp;
+    char output[BUFFER_SIZE];
+    char* full_output = NULL;
+    size_t total_size = 0;
+
+    printf ("Lancement du scanner avec la commande : %s\n", command);
+
+    char cmd_with_redirect[BUFFER_SIZE];
+    snprintf (cmd_with_redirect, sizeof (cmd_with_redirect), "%s 2>&1", command);
+
+    fp = popen (cmd_with_redirect, "r");
+    if (fp == NULL) {
+        const char* error = "Erreur lors de l'exécution de la commande.\n";
+        SSL_write (ssl, error, strlen (error));
+        return;
+    }
+
+    while (fgets (output, sizeof (output), fp) != NULL) {
+        size_t len = strlen (output);
+        char* temp = realloc (full_output, total_size + len + 1);
+        if (temp == NULL) {
+            free (full_output);
+            const char* error = "Erreur d’allocation mémoire.\n";
+            SSL_write (ssl, error, strlen (error));
+            pclose (fp);
+            return;
+        }
+
+        full_output = temp;
+        memcpy (full_output + total_size, output, len);
+        total_size += len;
+        full_output[total_size] = '\0';
+    }
+
+    pclose (fp);
+
+    if (total_size == 0) {
+        const char* msg = "Aucune sortie de la commande.\n";
+        SSL_write (ssl, msg, strlen (msg));
+    } else {
+        SSL_write (ssl, full_output, total_size);
+    }
+
+    free (full_output);
+}
+
+
+void func (SSL* ssl) {
     char buff[BUFFER_SIZE];
     while (1) {
-        bzero(buff, BUFFER_SIZE);
-        int bytes = SSL_read(ssl, buff, sizeof(buff));
-        if (bytes <= 0) break;
+        bzero (buff, BUFFER_SIZE);
+        int bytes = SSL_read (ssl, buff, sizeof (buff));
+        if (bytes <= 0)
+            break;
 
-        printf("Reçu de l'orchestrateur : %s", buff);
-        FILE* fp = popen(buff, "r");
-        bzero(buff, BUFFER_SIZE);
-
-        bool valid_command = false;
-        while (fgets(buff, sizeof(buff), fp) != NULL) {
-            SSL_write(ssl, buff, strlen(buff));
-            valid_command = true;
+        printf ("Reçu de l'orchestrateur : %s", buff);
+        buff[bytes] = '\0';
+        if (strstr (buff, "nmap") != NULL) {
+            run_scanner (ssl, buff);
+        } else {
+            const char* msg = "Error idk";
+            SSL_write (ssl, msg, strlen (msg));
         }
-
-        if (!valid_command) {
-            const char* error_message = "La commande envoyée n'est pas valide !\n";
-            SSL_write(ssl, error_message, strlen(error_message));
-        }
-
-        pclose(fp);
     }
 }
 
-int main(int argc, char* argv[]) {
+int main (int argc, char* argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        fprintf (stderr, "Usage: %s <port>\n", argv[0]);
+        exit (EXIT_FAILURE);
     }
-    int port = atoi(argv[1]);
+    int port = atoi (argv[1]);
 
     // OpenSSL setup
-    SSL_library_init();
-    SSL_load_error_strings();
-    OpenSSL_add_all_algorithms();
+    SSL_library_init ();
+    SSL_load_error_strings ();
+    OpenSSL_add_all_algorithms ();
 
-    SSL_CTX* ctx = SSL_CTX_new(TLS_server_method());
+    SSL_CTX* ctx = SSL_CTX_new (TLS_server_method ());
     if (!ctx) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
 
     // Load agent's certificate and private key
-    if (SSL_CTX_use_certificate_file(ctx, "certs/agent-cert.pem", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+    if (SSL_CTX_use_certificate_file (ctx, "certs/agent-cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
-    if (SSL_CTX_use_PrivateKey_file(ctx, "certs/agent-key.pem", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+    if (SSL_CTX_use_PrivateKey_file (ctx, "certs/agent-key.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
 
     // Verify that the private key matches the certificate
-    if (!SSL_CTX_check_private_key(ctx)) {
-        fprintf(stderr, "La clé privée ne correspond pas au certificat public\n");
-        exit(EXIT_FAILURE);
+    if (!SSL_CTX_check_private_key (ctx)) {
+        fprintf (stderr, "La clé privée ne correspond pas au certificat public\n");
+        exit (EXIT_FAILURE);
     }
 
     // Load CA certificate to verify the orchestrator's certificate
-    if (SSL_CTX_load_verify_locations(ctx, "ca/ca-cert.pem", NULL) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+    if (SSL_CTX_load_verify_locations (ctx, "ca/ca-cert.pem", NULL) <= 0) {
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
 
     // Set the verification mode to verify the orchestrator's certificate
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
-    SSL_CTX_set_verify_depth(ctx, 1);
+    SSL_CTX_set_verify (ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify_depth (ctx, 1);
 
     // TCP socket setup
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int serverSocket = socket (AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serv_addr, cli_addr;
-    socklen_t clilen = sizeof(cli_addr);
+    socklen_t clilen = sizeof (cli_addr);
 
-    bzero(&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
+    bzero (&serv_addr, sizeof (serv_addr));
+    serv_addr.sin_family      = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(port);
+    serv_addr.sin_port        = htons (port);
 
-    if (bind(serverSocket, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Erreur bind");
-        exit(1);
+    if (bind (serverSocket, (struct sockaddr*)&serv_addr, sizeof (serv_addr)) < 0) {
+        perror ("Erreur bind");
+        exit (1);
     }
 
-    if (listen(serverSocket, SOMAXCONN) < 0) {
-        perror("Erreur listen");
-        exit(1);
+    if (listen (serverSocket, SOMAXCONN) < 0) {
+        perror ("Erreur listen");
+        exit (1);
     }
 
-    printf("Agent écoutant sur le port %d...\n", port);
+    printf ("Agent écoutant sur le port %d...\n", port);
 
-    int clientSocket = accept(serverSocket, (struct sockaddr*)&cli_addr, &clilen);
+    int clientSocket = accept (serverSocket, (struct sockaddr*)&cli_addr, &clilen);
     if (clientSocket < 0) {
-        perror("Erreur accept");
-        exit(1);
+        perror ("Erreur accept");
+        exit (1);
     }
 
-    printf("Orchestrateur connecté !\n");
+    printf ("Orchestrateur connecté !\n");
 
     // TLS handshake
-    SSL* ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, clientSocket);
-    if (SSL_accept(ssl) <= 0) {
-        ERR_print_errors_fp(stderr);
-        close(clientSocket);
-        SSL_free(ssl);
-        SSL_CTX_free(ctx);
+    SSL* ssl = SSL_new (ctx);
+    SSL_set_fd (ssl, clientSocket);
+    if (SSL_accept (ssl) <= 0) {
+        ERR_print_errors_fp (stderr);
+        close (clientSocket);
+        SSL_free (ssl);
+        SSL_CTX_free (ctx);
         return 1;
     }
 
     // Verify the orchestrator's certificate
-    X509* cert = SSL_get_peer_certificate(ssl);
+    X509* cert = SSL_get_peer_certificate (ssl);
     if (cert == NULL) {
-        fprintf(stderr, "L'orchestrateur n'a pas fourni de certificat !\n");
-        SSL_shutdown(ssl);
-        SSL_free(ssl);
-        close(clientSocket);
-        SSL_CTX_free(ctx);
+        fprintf (stderr, "L'orchestrateur n'a pas fourni de certificat !\n");
+        SSL_shutdown (ssl);
+        SSL_free (ssl);
+        close (clientSocket);
+        SSL_CTX_free (ctx);
         return 1;
     }
 
     // Check if the orchestrator's certificate is valid
-    if (SSL_get_verify_result(ssl) != X509_V_OK) {
-        fprintf(stderr, "Le certificat de l'orchestrateur est invalide !\n");
-        X509_free(cert);
-        SSL_shutdown(ssl);
-        SSL_free(ssl);
-        close(clientSocket);
-        SSL_CTX_free(ctx);
+    if (SSL_get_verify_result (ssl) != X509_V_OK) {
+        fprintf (stderr, "Le certificat de l'orchestrateur est invalide !\n");
+        X509_free (cert);
+        SSL_shutdown (ssl);
+        SSL_free (ssl);
+        close (clientSocket);
+        SSL_CTX_free (ctx);
         return 1;
     }
 
-    printf("Certificat de l'orchestrateur vérifié avec succès !\n");
-    X509_free(cert);
+    printf ("Certificat de l'orchestrateur vérifié avec succès !\n");
+    X509_free (cert);
 
-    func(ssl);
+    func (ssl);
 
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-    close(clientSocket);
-    close(serverSocket);
-    SSL_CTX_free(ctx);
+    SSL_shutdown (ssl);
+    SSL_free (ssl);
+    close (clientSocket);
+    close (serverSocket);
+    SSL_CTX_free (ctx);
 
     return 0;
 }
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,5 +1,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,145 +9,151 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
 
 #define BUFFER_SIZE 1000
 #define MAX_AGENT_NAME 50
-#define NUM_AGENTS 3
+#define NUM_AGENTS 1
 
 struct sockaddr_in serv_addr[NUM_AGENTS];
 char agent_names[NUM_AGENTS][MAX_AGENT_NAME];
 
-void send_command(SSL* ssl, const char* agent_name) {
+void send_command (SSL* ssl, const char* agent_name) {
     char buff[BUFFER_SIZE];
     int n;
-    bzero(buff, sizeof(buff));
-    printf("Commande pour %s : ", agent_name);
+    bzero (buff, sizeof (buff));
+    printf ("Commande pour %s : ", agent_name);
     n = 0;
-    while ((buff[n++] = getchar()) != '\n') {}
+    while ((buff[n++] = getchar ()) != '\n') {
+    }
 
-    SSL_write(ssl, buff, strlen(buff));
-    bzero(buff, sizeof(buff));
-    SSL_read(ssl, buff, sizeof(buff));
-    printf("Réponse de %s : %s", agent_name, buff);
+    SSL_write (ssl, buff, strlen (buff));
+    bzero (buff, sizeof (buff));
+    SSL_read (ssl, buff, sizeof (buff));
+    printf ("Réponse de %s : %s", agent_name, buff);
 
-    if ((strncmp(buff, "exit", 4)) == 0) {
-        printf("Fermeture de la connexion avec %s...\n", agent_name);
+    if ((strncmp (buff, "exit", 4)) == 0) {
+        printf ("Fermeture de la connexion avec %s...\n", agent_name);
     }
 }
 
-int main(int argc, char** argv) {
+int main (int argc, char** argv) {
     if (argc < NUM_AGENTS + 2 || argc > 2 * NUM_AGENTS + 2) {
-        fprintf(stderr, "Usage: %s <IP1> <IP2> <IP3> <port> [name1 name2 name3]\n", argv[0]);
-        exit(EXIT_FAILURE);
+        fprintf (stderr,
+        "Usage: %s <IP1> <IP2> <IP3> <port> [name1 name2 name3]\n", argv[0]);
+        exit (EXIT_FAILURE);
     }
 
-    int port = atoi(argv[NUM_AGENTS + 1]);
+    int port = atoi (argv[NUM_AGENTS + 1]);
     int sockfds[NUM_AGENTS];
     SSL* ssl_connections[NUM_AGENTS];
 
     // Optional agent names
     for (int i = 0; i < NUM_AGENTS; ++i) {
         if (argc > NUM_AGENTS + 2 + i) {
-            strncpy(agent_names[i], argv[NUM_AGENTS + 2 + i], MAX_AGENT_NAME - 1);
+            strncpy (agent_names[i], argv[NUM_AGENTS + 2 + i], MAX_AGENT_NAME - 1);
             agent_names[i][MAX_AGENT_NAME - 1] = '\0';
         } else {
-            snprintf(agent_names[i], MAX_AGENT_NAME, "Agent %d", i + 1);
+            snprintf (agent_names[i], MAX_AGENT_NAME, "Agent %d", i + 1);
         }
     }
 
     // Init OpenSSL
-    SSL_library_init();
-    SSL_load_error_strings();
-    OpenSSL_add_all_algorithms();
+    SSL_library_init ();
+    SSL_load_error_strings ();
+    OpenSSL_add_all_algorithms ();
 
-    SSL_CTX* ctx = SSL_CTX_new(TLS_client_method());
+    SSL_CTX* ctx = SSL_CTX_new (TLS_client_method ());
     if (ctx == NULL) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
 
     // Load the certificate and private key for the orchestrator
-    if (SSL_CTX_use_certificate_file(ctx, "certs/orchestrator-cert.pem", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+    if (SSL_CTX_use_certificate_file (
+        ctx, "certs/orchestrator-cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "certs/orchestrator-key.pem", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+    if (SSL_CTX_use_PrivateKey_file (ctx, "certs/orchestrator-key.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp (stderr);
+        exit (EXIT_FAILURE);
     }
 
     // Verify the private key matches the certificate
-    if (!SSL_CTX_check_private_key(ctx)) {
-        fprintf(stderr, "La clé privée ne correspond pas au certificat.\n");
-        exit(EXIT_FAILURE);
+    if (!SSL_CTX_check_private_key (ctx)) {
+        fprintf (stderr, "La clé privée ne correspond pas au certificat.\n");
+        exit (EXIT_FAILURE);
     }
 
     // Setup and connect to each agent with SSL
     for (int i = 0; i < NUM_AGENTS; ++i) {
-        sockfds[i] = socket(PF_INET, SOCK_STREAM, 0);
+        sockfds[i] = socket (PF_INET, SOCK_STREAM, 0);
         if (sockfds[i] < 0) {
-            perror("Erreur socket");
-            exit(EXIT_FAILURE);
+            perror ("Erreur socket");
+            exit (EXIT_FAILURE);
         }
 
-        bzero(&serv_addr[i], sizeof(serv_addr[i]));
-        serv_addr[i].sin_family = AF_INET;
-        serv_addr[i].sin_port = htons((ushort)port);
-        serv_addr[i].sin_addr.s_addr = inet_addr(argv[i + 1]);
+        bzero (&serv_addr[i], sizeof (serv_addr[i]));
+        serv_addr[i].sin_family      = AF_INET;
+        serv_addr[i].sin_port        = htons ((ushort)port);
+        serv_addr[i].sin_addr.s_addr = inet_addr (argv[i + 1]);
 
-        if (connect(sockfds[i], (struct sockaddr*)&serv_addr[i], sizeof(serv_addr[i])) < 0) {
-            fprintf(stderr, "Erreur de connexion à %s (%s)\n", agent_names[i], argv[i + 1]);
-            close(sockfds[i]);
-            exit(EXIT_FAILURE);
+        if (connect (sockfds[i], (struct sockaddr*)&serv_addr[i],
+            sizeof (serv_addr[i])) < 0) {
+            fprintf (stderr, "Erreur de connexion à %s (%s)\n", agent_names[i],
+            argv[i + 1]);
+            close (sockfds[i]);
+            exit (EXIT_FAILURE);
         }
 
         // Setup SSL over the connected socket
-        SSL* ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, sockfds[i]);
+        SSL* ssl = SSL_new (ctx);
+        SSL_set_fd (ssl, sockfds[i]);
 
-        if (SSL_connect(ssl) <= 0) {
-            fprintf(stderr, "Échec SSL avec %s (%s)\n", agent_names[i], argv[i + 1]);
-            ERR_print_errors_fp(stderr);
-            SSL_free(ssl);
-            close(sockfds[i]);
-            exit(EXIT_FAILURE);
+        if (SSL_connect (ssl) <= 0) {
+            fprintf (stderr, "Échec SSL avec %s (%s)\n", agent_names[i], argv[i + 1]);
+            ERR_print_errors_fp (stderr);
+            SSL_free (ssl);
+            close (sockfds[i]);
+            exit (EXIT_FAILURE);
         }
 
         ssl_connections[i] = ssl;
 
-        printf("Connecté en TLS à %s (%s) avec succès !\n", agent_names[i], argv[i + 1]);
+        printf ("Connecté en TLS à %s (%s) avec succès !\n", agent_names[i], argv[i + 1]);
     }
 
     // Command loop
     while (1) {
         int target;
-        printf("\nÀ quel agent voulez-vous envoyer la commande ? (1 à %d, ou 0 pour quitter): ", NUM_AGENTS);
-        scanf("%d", &target);
-        while (getchar() != '\n'); // Clear input buffer
+        printf ("\nÀ quel agent voulez-vous envoyer la commande ? (1 à %d, ou "
+                "0 pour quitter): ",
+        NUM_AGENTS);
+        scanf ("%d", &target);
+        while (getchar () != '\n')
+            ; // Clear input buffer
 
         if (target == 0) {
-            printf("Fermeture des connexions...\n");
+            printf ("Fermeture des connexions...\n");
             break;
         }
 
         if (target < 1 || target > NUM_AGENTS) {
-            printf("Numéro d'agent invalide.\n");
+            printf ("Numéro d'agent invalide.\n");
             continue;
         }
 
-        send_command(ssl_connections[target - 1], agent_names[target - 1]);
+        send_command (ssl_connections[target - 1], agent_names[target - 1]);
     }
 
     // Cleanup
     for (int i = 0; i < NUM_AGENTS; ++i) {
-        SSL_shutdown(ssl_connections[i]);
-        SSL_free(ssl_connections[i]);
-        close(sockfds[i]);
+        SSL_shutdown (ssl_connections[i]);
+        SSL_free (ssl_connections[i]);
+        close (sockfds[i]);
     }
-    SSL_CTX_free(ctx);
-    EVP_cleanup();
+    SSL_CTX_free (ctx);
+    EVP_cleanup ();
     return 0;
 }
